d991db9e8fe0f2b02c7621f6bd0776306df2c8ac
Paul Eriksson <pauleriksson@localadmins-MacBook-Pro.local>
2018-05-17 14:13:27 +0300

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+      'gatsby-plugin-react-helmet',
+      {
+        resolve: 'gatsby-plugin-postcss-sass',
+        options: {
+          precision: 8,
+        },
+    },
+  ],
+};
+
