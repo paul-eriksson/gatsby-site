eccc7f0edcb88b3260a800e9e7b227ccdfcb4778
Paul Eriksson <pauleriksson@localadmins-MacBook-Pro.local>
2018-05-17 15:05:47 +0300

    added navbar from other projects + created pages for navigation links

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import Link from 'gatsby-link';
 
-import Header from '../components/header';
+import FaHome from 'react-icons/lib/fa/home';
+import FaUser from 'react-icons/lib/fa/user';
+import FaPencil from 'react-icons/lib/fa/pencil';
+import FaStar from 'react-icons/lib/fa/star';
+import FaFolderOpen from 'react-icons/lib/fa/folder-open';
+import WebFont from 'webfontloader';
+
+import IconText from '../components/IconText';
+import logo from '../images/logo.svg';
+import Navbar from '../components/Navbar';
 import './index.scss';
+
+
+WebFont.load({
+  google: {
+    families: ['Titillium Web:300,400,700', 'sans-serif'],
+  },
+});
 
 const Layout = ({ children, data }) => (
   <div>
@@ -14,7 +31,13 @@ const Layout = ({ children, data }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
+    <Navbar icon={<img src={logo} alt="logo" />}>
+      <Link to="/"><IconText text="Home" icon={<FaHome size={27} />} /></Link>
+      <Link to="/about/"><IconText text="About" icon={<FaUser size={27} />} /></Link>
+      <Link to="/skills"><IconText text="Skills" icon={<FaStar size={27} />} /></Link>
+      <Link to="/work/"><IconText text="Work" icon={<FaFolderOpen size={27} />} /></Link>
+      <Link to="/contact/"><IconText text="Contact" icon={<FaPencil size={27} />} /></Link>
+    </Navbar>
     <div
       style={{
         margin: '0 auto',
@@ -43,3 +66,6 @@ export const query = graphql`
     }
   }
 `;
+
+
+// <Header siteTitle={data.site.siteMetadata.title} />
---
+++ src/pages/skills.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+const Skills = () => (
+  <div>
+    <h1>Hi from the skills page</h1>
+    <p>Welcome to skills</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+);
+
+export default Skills;
