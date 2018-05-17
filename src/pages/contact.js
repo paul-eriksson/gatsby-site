7ed680ca184093ce145d26ca5bc3ce635ef9931a
Paul Eriksson <pauleriksson@localadmins-MacBook-Pro.local>
2018-05-17 15:51:22 +0300

    added google maps on contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import GoogleMaps from '../components/GoogleMaps';
+import './contact.scss';
 
 const Contact = () => (
-  <div>
-    <h1>Hi from the contact page</h1>
-    <p>Welcome to contact</p>
-    <Link to="/">Go back to the homepage</Link>
+  <div className="contact__container">
+    <h1 className="contact__title">Contact</h1>
+    <div className="contact__map">
+      <GoogleMaps
+        isMarkerShown
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: '100%' }} />}
+        containerElement={<div style={{ height: '100%', width: '100%' }} />}
+        mapElement={<div style={{ height: '100%' }} />}
+      />
+    </div>
   </div>
 );
 
