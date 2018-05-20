2c8e9adcc1cfa65551ea877a568c16ac0311e2b1
Paul Eriksson <pauleriksson@localadmins-MacBook-Pro.local>
2018-05-21 01:46:12 +0300

    minor page style changes

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import StyledMap from './GoogleMapsStyle.json';
 
 const GoogleMaps = withScriptjs(withGoogleMap(props => (
     <GoogleMap
-      defaultZoom={8}
-      defaultCenter={{ lat: -34.397, lng: 150.644 }}
+      defaultZoom={5}
+      defaultCenter={{ lat: 60.189157, lng: 24.939011 }}
+      defaultOptions={{ styles: StyledMap }}
     >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+    {props.isMarkerShown && <Marker position={{ lat: 60.189157, lng: 24.939011 }} />}
     </GoogleMap>
 )));
 
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -44,6 +44,9 @@ const Layout = ({ children, data }) => (
         maxWidth: 960,
         padding: '0px 1.0875rem 1.45rem',
         paddingTop: 0,
+        backgroundColor: '#323438',
+        height: '100vh',
+        color: 'white',
       }}
     >
       {children()}
