a4a7b65b3846034a9dfaf972083721ff1563bf9b
Paul Eriksson <pauleriksson@localadmins-MacBook-Pro.local>
2018-05-21 10:22:14 +0300

    correct colours added for burger menu navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,20 +28,27 @@ class NavBar extends React.Component{
                 </div>
                 <div className="navbar__child navbar__child--right">
                     <nav className="navbar__nav navbar__nav--wide">
-                    {this.props.children.map((navItem, index) => {
-                        return (
-                            React.cloneElement(navItem, {
-                                onClick: () => this.NavClick(index),
-                                className: this.state.currentPage === index ? 'navbar__nav-item navbar__nav-item--current':'navbar__nav-item navbar__nav-item--other',
-                            })
-                        );
-                     })}
+                        {this.props.children.map((navItem, index) => {
+                            return (
+                                React.cloneElement(navItem, {
+                                    onClick: () => this.NavClick(index),
+                                    className: this.state.currentPage === index ? 'navbar__nav-item navbar__nav-item--current':'navbar__nav-item navbar__nav-item--other',
+                                })
+                            );
+                        })}
                     </nav>
                     <nav className="navbar__nav navbar__nav--narrow">
                         <FaBars size="2.5em" className="navbar__navicon" onClick={() => (this.setState({ burgerToggle: !this.state.burgerToggle }))} />
                         {this.state.burgerToggle ?
                             <div className="navbar__navlinks navbar__navlinks--narrow">
-                                {this.props.children}
+                                {this.props.children.map((navItem, index) => {
+                                    return (
+                                        React.cloneElement(navItem, {
+                                            onClick: () => this.NavClick(index),
+                                            className: this.state.currentPage === index ? 'navbar__nav-item navbar__nav-item--current':'navbar__nav-item navbar__nav-item--other',
+                                        })
+                                    );
+                                })}
                             </div> : null
                         }
                     </nav>
